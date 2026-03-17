ec3ad8139b951b35d4c61b6b15dd2e44f3becfd2
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-16 23:48:29 -0700

    Made filename formatter

---
+++ format_filenames.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+def format_filenames(path: str=None):
+    while path is None or not Path(path).exists():
+        path = input("Filepath: ")
+
+    files = Path(path).glob("*.*")
+
+    for file in files:
+        original_filename = file.name
+        formatted_name = \
+            f"{original_filename.lower().strip().
+            replace(" ", "_").
+            replace("\'", "_").
+            replace("&", "and").
+            replace(",", "")}"
+
+        os.rename(file.absolute(), formatted_name)
+
+        print(f"Renamed {file} to {formatted_name}")
+
+if __name__ == "__main__":
+    format_filenames()
