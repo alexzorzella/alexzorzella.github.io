f6a871180f5d9a050fe4e54c21ebbb0995a9b445
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-16 23:48:29 -0700

    Made filename formatter

--- cards.generate.py
+++ cards.generate.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
-def main():
+def populate_card_page(template:str="metafight_cards.template.html", output:str="cards.html", cards:str="public/metafight"):
     project_root = Path(__file__).parent
-    template_file = project_root / "cards.template.html"
-    output_file = project_root / "cards.html"
+    template_file = project_root / template
+    output_file = project_root / output
 
-    cards_dir = project_root / "public/metafight"
+    cards_dir = project_root / cards
 
     card_files = cards_dir.glob("*.png")
     code_gen = ""
@@ -13,7 +13,7 @@ def main():
     code_gen += "<ul>\n"
 
     for card_file in card_files:
-        code_gen += f'<li><img src="/public/metafight/{card_file.name}" alt="{card_file.name}"></li>\n'
+        code_gen += f'<li><img src="{cards}{card_file.name}" alt="{card_file.name}"></li>\n'
 
     code_gen += "</ul>\n"
 
@@ -25,4 +25,5 @@ def main():
         output.write(template)
 
 if __name__ == "__main__":
-    main()
+    populate_card_page()
+    populate_card_page(template="magic_cards.template.html", output="mtg.html", cards="public/mtg")
