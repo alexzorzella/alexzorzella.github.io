2294ebb00e6bc8a1e280b069513572fef1abb28a
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-16 19:56:28 -0700

    Updated marquee

--- convert.sh
+++ convert.sh
@@ -2,14 +2,18 @@
 
 set -e
 
+echo "Converting .jpg files in public/art to .png"
+
 for f in public/art/*.jpg; do
     convert "$f" "public/art/$(basename "${f%.jpg}").png"
 done
+
+echo "Clearing public/art/thumbnails"
 
 rm -r public/art/thumbnails
 mkdir -p public/art/thumbnails
 
 for f in public/art/*.png; do
-    echo $f
+    echo "Converting $f"
     convert "$f" -resize x512\> "public/art/thumbnails/$(basename "${f%.*}").webp"
 done
