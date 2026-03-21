0f31affa98efcce727d4fc96b4bf69ab33b5e9d8
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-21 14:52:53 -0700

    Added pdf_to_png.py

---
+++ pdf_to_png.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from pdf2image import convert_from_path
+
+def format_filenames(path: str = None):
+    """Saves the pages of all PDF files in a directory to PNG files"""
+    while path is None or not Path(path).exists():
+        path = input("Filepath: ")
+
+    pdf_files = Path(path).glob("*.pdf")
+    output_dir = path / "output"
+
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    for pdf in pdf_files:
+        images = convert_from_path(pdf, dpi=300)
+
+        for i, image in enumerate(images):
+            image_filename = f"{Path(pdf).stem}_{i}.png"
+            image.save(Path(output_dir) / image_filename, 'PNG')
+
+        print(f"Saved {len(images)} from {pdf}")
+
+    print("Done!")
+
+if __name__ == "__main__":
+    format_filenames()
