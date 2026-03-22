630c9b944859662ab9636788c7d579692ee6e59e
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-21 20:23:27 -0700

    populate_webpages maps the creation of the thumbnails to process the files very fast

--- populate_webpages.py
+++ populate_webpages.py
@@ -1,7 +1,10 @@
+import itertools
+import os
 from pathlib import Path
 from PIL import Image
+from multiprocessing import Pool
 
-def populate_gallery(template:str, output:str, artwork_directory:str):
+def populate_gallery(template_file:str, output_file:str, artwork_directory:str):
     code_gen = ""
 
     subdirectories = Path(artwork_directory).iterdir()
@@ -23,18 +26,20 @@ def populate_gallery(template:str, output:str, artwork_directory:str):
             code_gen += f"""
         <li class="tile"><a href={formatted_image_path}><img fetchpriority="high" src={thumbnail_path} alt="Art"/></a></li>"""
 
-    with open(template, "r") as template_file:
-        template = template_file.read()
+    with open(template_file, "r") as file:
+        template = file.read()
         template = template.replace("__TEMPLATE__", code_gen)
 
-    with open(output, "w") as output:
-        output.write(template)
+    with open(output_file, "w") as output_file:
+        output_file.write(template)
 
-def populate_card_page(template:str, output:str, card_dir:str):
+    print(f"Populated {Path(template_file).stem} with cards from {artwork_directory}")
+
+def populate_card_page(template_file:str, output:str, card_dir:str):
     """Creates a populated HTML file given a template, output, and a local directory of cards"""
 
     project_root = Path(__file__).parent
-    template_file = project_root / template
+    template_file = project_root / template_file
     output_file = project_root / output
 
     cards_dir = project_root / card_dir
@@ -56,40 +61,44 @@ def populate_card_page(template:str, output:str, card_dir:str):
 
     code_gen += "</ul>\n"
 
-    with open(template_file, "r") as template_file:
-        template = template_file.read()
+    with open(template_file, "r") as file:
+        template = file.read()
         template = template.replace("__TEMPLATE__", code_gen)
 
     with open(output_file, "w") as output:
         output.write(template)
 
-def compress_images_into_thumbnails_recursively(parent_directory:str, thumbnail_height:int=512):
+    print(f"Populated {template_file.stem} with cards from {card_dir}")
+
+def create_thumbnail_for_image(image_path:str, thumbnail_height:int, output_directory:str):
+    with Image.open(image_path) as image:
+        width, height = image.size
+
+        aspect_ratio = width / height
+        new_width = int(thumbnail_height * aspect_ratio)
+
+        resized_image = image.resize((new_width, thumbnail_height), Image.Resampling.LANCZOS)
+
+        output_path = output_directory / f"{image_path.stem}.webp"
+        resized_image.save(output_path, "WEBP", quality=100)
+
+def create_thumbnails_for_images_recursively(parent_directory:str, thumbnail_height:int=512):
     output_directory = Path(parent_directory) / "thumbnails"
     Path(output_directory).mkdir(parents=True, exist_ok=True)
 
-    total_images_processed = 0
+    images = Path(parent_directory).rglob("*.png")
 
-    for image_path in Path(parent_directory).rglob("*.png"):
-        with Image.open(image_path) as image:
-            width, height = image.size
+    args = [(image_path, thumbnail_height, output_directory) for image_path in images]
 
-            aspect_ratio = width / height
-            new_width = int(thumbnail_height * aspect_ratio)
+    with Pool(processes=os.cpu_count()) as pool:
+        pool.starmap(create_thumbnail_for_image, args)
 
-            resized_image = image.resize((new_width, thumbnail_height), Image.Resampling.LANCZOS)
-
-            output_path = output_directory / f"{image_path.stem}.webp"
-            resized_image.save(output_path, "WEBP", quality=100)
-
-            print(f"Saved thumbnail for {image_path} to {output_path}")
-            total_images_processed += 1
-
-    print(f"Processed {total_images_processed} images")
+    print(f"Done! {len(args)} thumbnails created for images in {parent_directory}")
 
 if __name__ == "__main__":
-    compress_images_into_thumbnails_recursively("public/art")
-    compress_images_into_thumbnails_recursively("public/metafight")
-    compress_images_into_thumbnails_recursively("public/mtg")
-    populate_gallery(template="index.template.html", output="index.html", artwork_directory="public/art")
-    populate_card_page(template="metafight_cards.template.html", output="cards.html", card_dir="public/metafight")
-    populate_card_page(template="magic_cards.template.html", output="mtg.html", card_dir="public/mtg")
+    create_thumbnails_for_images_recursively("public/art")
+    create_thumbnails_for_images_recursively("public/metafight")
+    create_thumbnails_for_images_recursively("public/mtg")
+    populate_gallery(template_file="index.template.html", output_file="index.html", artwork_directory="public/art")
+    populate_card_page(template_file="metafight_cards.template.html", output="cards.html", card_dir="public/metafight")
+    populate_card_page(template_file="magic_cards.template.html", output="mtg.html", card_dir="public/mtg")
