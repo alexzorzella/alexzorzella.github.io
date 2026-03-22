283d14fa97820e418543b3ef14d3cb3afdfa3560
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-03-21 21:40:09 -0700

    Migrating populate_webpages to store image templates in a dataclass that can be rendered
    Co-authored-by: Alex Ott <66271487+AlexanderHOtt@users.noreply.github.com>

--- populate_webpages.py
+++ populate_webpages.py
@@ -1,27 +1,45 @@
-import itertools
 import os
 from pathlib import Path
 from PIL import Image
 from multiprocessing import Pool
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class HTMLImageTemplate:
+    thumbnail_path: Path | None
+    asset_path: Path
+    alt:str
+
+    def render(self):
+        if self.thumbnail_path is not None:
+            return f'<li><a href="{self.asset_path.as_posix()}"><img src="{self.thumbnail_path.as_posix()}" alt="{self.alt}"></a></li>'
+        else:
+            return f'<li><a href="{self.asset_path.as_posix()}"><img src="{self.asset_path.as_posix()}" alt="{self.alt}"></a></li>'
 
 def populate_gallery(template_file:str, output_file:str, artwork_directory:str):
     code_gen = ""
 
+    project_root = Path(__file__).parent
+
+    directories = [Path(artwork_directory)]
+
     subdirectories = Path(artwork_directory).iterdir()
     subdirectories = sorted(subdirectories)
 
-    for subdirectory in subdirectories:
-        if not Path(subdirectory).is_dir() or subdirectory.name == "thumbnails":
+    directories.extend(subdirectories)
+
+    for directory in directories:
+        if not Path(directory).is_dir() or directory.name == "thumbnails":
             continue
 
-        artwork_files = subdirectory.glob(f"*.png")
+        artwork_files = directory.glob(f"*.png")
         artwork_files = sorted(artwork_files)
 
         for artwork_file in artwork_files:
-            image_path = artwork_file
+            image_path = f"{project_root}/{artwork_file.name}"
             thumbnail_path = f"public/art/thumbnails/{Path(image_path).stem}.webp"
 
-            formatted_image_path = str(image_path).replace("\\", "/")
+            formatted_image_path = image_path # .replace("\\", "/")
 
             code_gen += f"""
         <li class="tile"><a href={formatted_image_path}><img fetchpriority="high" src={thumbnail_path} alt="Art"/></a></li>"""
@@ -35,40 +53,53 @@ def populate_gallery(template_file:str, output_file:str, artwork_directory:str):
 
     print(f"Populated {Path(template_file).stem} with cards from {artwork_directory}")
 
-def populate_card_page(template_file:str, output:str, card_dir:str):
+def populate_card_page(output_template_filename:str, output_filename:str, card_source_directory_name:str):
     """Creates a populated HTML file given a template, output, and a local directory of cards"""
 
     project_root = Path(__file__).parent
-    template_file = project_root / template_file
-    output_file = project_root / output
+    output_template_path = project_root / output_template_filename
+    output_filepath = project_root / output_filename
 
-    cards_dir = project_root / card_dir
-    card_files = cards_dir.glob(f"*.png")
+    card_source_directory_path = project_root / card_source_directory_name
+
+    directories_to_process = [Path(card_source_directory_path)]
+
+    subdirectories = Path(card_source_directory_path).iterdir()
+    subdirectories = sorted(subdirectories)
+
+    directories_to_process.extend(subdirectories)
+
+    html_image_templates: list[HTMLImageTemplate] = []
+
+    for directory in directories_to_process:
+        if directory.name == "thumbnails":
+            continue
+
+        card_filepaths = directory.glob(f"*.png")
+
+        for card_filepath in card_filepaths:
+            thumbnail_path = Path(f'/{card_source_directory_name}/thumbnails/{card_filepath.name.replace(".png", ".webp")}')
+            asset_path = Path(f'/{card_source_directory_name}/{directory.relative_to(card_source_directory_path)}/{card_filepath.name}')
+
+            html_image_templates.append(HTMLImageTemplate(thumbnail_path=thumbnail_path, asset_path=asset_path, alt=card_filepath.stem))
 
     code_gen = ""
 
     code_gen += "<ul>\n"
 
-    for card_file in card_files:
-        thumbnail_path = f'{card_dir}/thumbnails/{card_file.name.replace(".png", ".webp")}'
-
-        if Path(thumbnail_path).exists():
-            image_source = thumbnail_path
-            code_gen += f'<li><a href="{card_dir}/{card_file.name}"><img src="{image_source}" alt="{card_file.name}"></a></li>\n'
-        else:
-            image_source = f"{card_dir}/{card_file.name}"
-            code_gen += f'<li><img src="{image_source}" alt="{card_file.name}"></li>\n'
+    for template in html_image_templates:
+        code_gen += template.render() + "\n"
 
     code_gen += "</ul>\n"
 
-    with open(template_file, "r") as file:
+    with open(output_template_path, "r") as file:
         template = file.read()
         template = template.replace("__TEMPLATE__", code_gen)
 
-    with open(output_file, "w") as output:
-        output.write(template)
+    with open(output_filepath, "w") as output_filename:
+        output_filename.write(template)
 
-    print(f"Populated {template_file.stem} with cards from {card_dir}")
+    print(f"Populated {output_template_path.stem} with cards from {card_source_directory_name}")
 
 def create_thumbnail_for_image(image_path:str, thumbnail_height:int, output_directory:str):
     with Image.open(image_path) as image:
@@ -96,9 +127,9 @@ def create_thumbnails_for_images_recursively(parent_directory:str, thumbnail_hei
     print(f"Done! {len(args)} thumbnails created for images in {parent_directory}")
 
 if __name__ == "__main__":
-    create_thumbnails_for_images_recursively("public/art")
-    create_thumbnails_for_images_recursively("public/metafight")
-    create_thumbnails_for_images_recursively("public/mtg")
+    # create_thumbnails_for_images_recursively("public/art")
+    # create_thumbnails_for_images_recursively("public/metafight")
+    # create_thumbnails_for_images_recursively("public/mtg")
     populate_gallery(template_file="index.template.html", output_file="index.html", artwork_directory="public/art")
-    populate_card_page(template_file="metafight_cards.template.html", output="cards.html", card_dir="public/metafight")
-    populate_card_page(template_file="magic_cards.template.html", output="mtg.html", card_dir="public/mtg")
+    populate_card_page(output_template_filename="metafight_cards.template.html", output_filename="cards.html", card_source_directory_name="public/metafight")
+    populate_card_page(output_template_filename="magic_cards.template.html", output_filename="mtg.html", card_source_directory_name="public/mtg")
