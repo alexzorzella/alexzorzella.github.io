33fa6b3f98c310aa09a8b4b0fa0ba50db6be9e89
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2026-05-13 22:06:40 -0700

    Added Lady Liberty and Gravity cards

--- populate_webpages.py
+++ populate_webpages.py
@@ -199,9 +199,9 @@ def create_page_for_subdirectory_in_directory(
 if __name__ == "__main__":
     # create_thumbnails_for_images_recursively("public/art")
     # create_thumbnails_for_images_recursively("public/metafight")
-    create_thumbnails_for_images_recursively("public/mtg")
+    # create_thumbnails_for_images_recursively("public/mtg")
     # create_thumbnails_for_images_recursively("public/fine_art_i_like")
-    # create_thumbnails_for_images_recursively("public/universes_beyond_logos")
+    create_thumbnails_for_images_recursively("public/universes_beyond_logos")
 
     # populate_template(
     #     output_template_filename="index.template.html",
@@ -219,21 +219,21 @@ if __name__ == "__main__":
     #     ul_class="tilelist",
     # )
 
-    create_page_for_subdirectory_in_directory(
-        parent_directory="public/mtg",
-        output_template_filename="mtg_cards.template.html",
-        output_to_directory="mtg_card_pages",
-        thumbnail_dir="/public/mtg/thumbnails"
-    )
-
-    # populate_template(
-    #     output_template_filename="mtg.template.html",
-    #     output_filename="mtg.html",
-    #     image_sources_directory_name="public/universes_beyond_logos",
-    #     li_class="real-size-tile",
-    #     ul_class="tilelist",
-    #     link_tiles_to_html_pages_of_the_same_name_in="/mtg_card_pages"
+    # create_page_for_subdirectory_in_directory(
+    #     parent_directory="public/mtg",
+    #     output_template_filename="mtg_cards.template.html",
+    #     output_to_directory="mtg_card_pages",
+    #     thumbnail_dir="/public/mtg/thumbnails"
     # )
+
+    populate_template(
+        output_template_filename="mtg.template.html",
+        output_filename="mtg.html",
+        image_sources_directory_name="public/universes_beyond_logos",
+        li_class="real-size-tile",
+        ul_class="tilelist",
+        link_tiles_to_html_pages_of_the_same_name_in="/mtg_card_pages"
+    )
 
     # populate_template(
     #     output_template_filename="metafight_cards.template.html",
